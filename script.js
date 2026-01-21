0fa5cb45900942764d58a82c7c3013b06dca331c
Aditya Singh <aditsingh2554@gmail.com>
2026-01-21 15:27:05 +0530

    Complete the whole project

---
+++ script.js
@@ -0,0 +1,28 @@
+ document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contactForm");
+  const status = document.getElementById("status");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // 🚫 stops page reload
+
+    const formData = new FormData(form);
+
+    fetch("https://sheetdb.io/api/v1/klxwoljtxs5p7", {
+      method: "POST",
+      body: formData
+    })
+    .then(() => {
+      status.textContent = "Form submitted successfully!";
+      form.reset();
+    })
+    .catch(() => {
+      status.textContent = "Something went wrong. Try again.";
+    });
+  });
+});
+
+const readMoreCard = document.getElementById("read-more-card");
+
+readMoreCard.addEventListener("click", () => {
+  readMoreCard.classList.toggle("fixed");
+});
